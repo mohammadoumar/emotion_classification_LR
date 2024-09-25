690fa2b67198f9ccc933c4e14f4070beb59200c8
mohammadoumar <umer.mushtaq@u-paris2.fr>
2024-09-25 16:09:21 +0200

    edit nb and script, run models

--- zeroshot/scripts/zeroshot_classification.py
+++ zeroshot/scripts/zeroshot_classification.py
@@ -3,6 +3,7 @@
 import os
 import ast
 import sys
+import json
 import torch
 import pickle
 import argparse
@@ -41,15 +42,19 @@ OUTPUT_DIR = Path(ZS_DIR) / "results" / f"zs_{model_id.split('/')[1]}"
 
 inference_tokenizer = AutoTokenizer.from_pretrained(model_id, padding='left', padding_side='left')
 inference_tokenizer.pad_token = inference_tokenizer.eos_token
-terminators = [inference_tokenizer.eos_token_id, inference_tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+#terminators = [inference_tokenizer.eos_token_id, inference_tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+#inference_tokenizer.chat_template = "llama"
 
 generation_model = AutoModelForCausalLM.from_pretrained(
     model_id,
     torch_dtype=torch.bfloat16,
-    # attn_implementation="flash_attention_2",
+    #attn_implementation="flash_attention_2",
     device_map="auto",
 )
 
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#generation_model.to(device)
+# print(generation_model.device)
 #generation_model = nn.DataParallel(generation_model)
 
 
@@ -88,8 +93,8 @@ prepared_sys_task_msg_l = []
 
 for row in df.iterrows():
 
-    sys_msg = {"role":"user", "content": "### Task description: You are an expert sentiment analysis assistant that takes an utterance from a comic book and must classify the utterance into appropriate emotion class(s): anger, surprise, fear, disgust, sadness, joy, neutral. You must absolutely not generate any text or explanation other than the following JSON format: {utterance_emotion: <predicted emotion classes for the utterance (str)>}\n\n"}
-    task_msg = {"role":"assistant", "content": f"# Utterance:\n{row[1].utterance}\n\n# Result:\n"}
+    sys_msg = {"role":"system", "content": "### Task description: You are an expert sentiment analysis assistant that takes an utterance from a comic book and must classify the utterance into appropriate emotion class(s): anger, surprise, fear, disgust, sadness, joy, neutral. You must absolutely not generate any text or explanation other than the following JSON format: {\"utterance_emotion\": \"<predicted emotion classes for the utterance (str)>}\"\n\n"}
+    task_msg = {"role":"user", "content": f"# Utterance:\n{row[1].utterance}\n\n# Result:\n"}
 
     sys_msg_l.append(sys_msg)
     task_msg_l.append(task_msg)
@@ -102,31 +107,98 @@ for i in range(len(sys_msg_l)):
 
 outputs_l = []
 
-for i in tqdm(range(len(prepared_sys_task_msg_l))):
+#for i in tqdm(range(len(prepared_sys_task_msg_l))):
 
-    messages = prepared_sys_task_msg_l[i]
+messages = prepared_sys_task_msg_l
 
-    input_ids = inference_tokenizer.apply_chat_template(
-    messages,
-    add_generation_prompt=True,
-    tokenize=True,
-    #tokenize=False,
-    padding=True,
-    truncation=True,
-    return_tensors="pt"
-).to(generation_model.device) # type: ignore
+input_ids = inference_tokenizer.apply_chat_template(
+messages,
+add_generation_prompt=True,
+tokenize=True,
+#tokenize=False,
+padding=True,
+truncation=True,
+#return_dict=True,
+return_tensors="pt"
+)#.to(generation_model.device)
+input_ids = torch.tensor(input_ids)
+# print(input_ids)
+print(input_ids.shape)
+# x = input_ids.ids[:32, :]
+# print(x.shape)
+# x.to(generation_model.device)
 
+BATCH_SIZE = 256
+n = len(prepared_sys_task_msg_l)
+
+for i in tqdm(range(0, n, BATCH_SIZE)):
+    #batch = input_ids[:BATCH_SIZE, :].to(generation_model.device) 
+    batch = input_ids[i:i + BATCH_SIZE, :].to(generation_model.device)
+    print(batch.shape)
     outputs = generation_model.generate(
-    input_ids = input_ids,
+    input_ids = batch,
     max_new_tokens=64,
     pad_token_id=inference_tokenizer.eos_token_id,
-    eos_token_id=terminators,
+    #eos_token_id=terminators,
     do_sample=True,
     temperature=0.1,
     top_p=0.9,
     )
-    # inference_tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
-    outputs_l.append(inference_tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True))
+    outputs = [inference_tokenizer.decode(output[input_ids.shape[-1]:], skip_special_tokens=True) for output in outputs]
+    outputs_l.append(outputs)
+    #outputs_l.append(inference_tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)) 
+    del batch
+
+print(len(outputs_l))
+
+
+# outputs = generation_model.generate(
+#     input_ids = input_ids,
+#     max_new_tokens=64,
+#     pad_token_id=inference_tokenizer.eos_token_id,
+#     eos_token_id=terminators,
+#     do_sample=True,
+#     temperature=0.1,
+#     top_p=0.9,
+#     )
+
+# for i in tqdm(range(len(prepared_sys_task_msg_l))):
+
+#     messages = prepared_sys_task_msg_l[i]
+
+#     input_ids = inference_tokenizer.apply_chat_template(
+#     messages,
+#     add_generation_prompt=True,
+#     tokenize=True,
+#     #tokenize=False,
+#     padding=True,
+#     truncation=True,
+#     #return_dict=True,
+#     return_tensors="pt"
+# )#.to(generation_model.device) 
+#     x = torch.tensor(input_ids).to(generation_model.device)
+#     #y = torch.tensor(input_ids["attention_mask"]).to(generation_model.device)
+#     #print(x)
+#     #print(y)
+#     #break
+#     #idz = x.to(generation_model.device)
+#     #x = torch.tensor(input_ids.)
+#     #x.to(generation_model.device)
+#     #print(x)
+#     #print(type(x))
+#     #print(x.device)
+#     #break
+#     outputs = generation_model.generate(
+#     input_ids = x,
+#     max_new_tokens=64,
+#     pad_token_id=inference_tokenizer.eos_token_id,
+#     eos_token_id=terminators,
+#     do_sample=True,
+#     temperature=0.1,
+#     top_p=0.9,
+#     )
+#     # inference_tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
+#     outputs_l.append(inference_tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True))  # type: ignore
 
 ### 6. Save Results, Post Process and Save Classficiation Reports ###
 
@@ -143,7 +215,31 @@ with results_file.open('wb') as fh:
     }
     pickle.dump(results_d, fh)
 
-preds = [list(ast.literal_eval(output).values()) for output in outputs_l]
+preds = []
+for output in outputs_l:
+    for prediction in output:
+        preds.append([list(ast.literal_eval(prediction).values())])
+
+#preds = [list(ast.literal_eval(output).values()) for output in outputs_l]
+
+# preds = []
+
+# for output in outputs_l:
+
+#     print(len(output))
+
+#     for prediction in output:
+
+#         emotion_data = json.loads(prediction)
+        
+#         # Split by comma and strip any extra whitespace
+#         emotions = [emotion.strip() for emotion in emotion_data["utterance_emotion"].split(',')]
+        
+#         # Append each emotion list to the final list
+#         preds.append(emotions)
+
+print(len(preds))
+
 grounds_matrix, preds_matrix = post_process_zs(grounds, preds)
 
 print(classification_report(grounds_matrix, preds_matrix, target_names=all_labels, digits=3))
