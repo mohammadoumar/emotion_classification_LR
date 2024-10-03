523bbec9007a9853185f22207f92a15068e2db7f
mohammadoumar <umer.mushtaq@u-paris2.fr>
2024-10-03 17:30:54 +0200

    add eval nb, parse results script

---
+++ incontext_learning/utils/parse_results.py
@@ -0,0 +1,35 @@
+import os
+import json
+import pickle
+
+from pathlib import Path
+
+all_reports = {}
+
+CURRENT_DIR = Path.cwd()
+ICL_DIR = CURRENT_DIR.parent
+RESULTS_DIR = os.path.join(ICL_DIR, "results")
+
+
+directories = [d for d in os.listdir(RESULTS_DIR) if os.path.isdir(os.path.join(RESULTS_DIR, d))]
+
+for directory in directories:
+
+    for file_name in os.listdir(os.path.join(RESULTS_DIR, directory)):
+
+        if file_name.startswith('classification_report') and file_name.endswith('.pickle'):
+            file_path = os.path.join(RESULTS_DIR, directory, file_name)
+            
+
+            with open(file_path, 'rb') as f:
+                classification_report = pickle.load(f)
+            
+
+            all_reports[directory] = classification_report
+
+with open(os.path.join(RESULTS_DIR, 'icl_classification_reports.json'), 'w') as json_file:
+    json.dump(all_reports, json_file, indent=4)
+
+print("Classification reports have been successfully dumped into 'icl_classification_reports.json'")
+
+
