3e3d46bc55659b281452471db00a8109dbc188e5
mohammadoumar <umer.mushtaq@u-paris2.fr>
2024-10-21 17:41:54 +0200

    update zs classification and post-process script

--- zeroshot/scripts/zeroshot_classification_cp.py
+++ zeroshot/scripts/zeroshot_classification_cp.py
@@ -35,8 +35,6 @@ ZS_DIR = CURRENT_DIR.parent
 DATASET_DIR = Path(ZS_DIR).parent / "dataset_files"
 OUTPUT_DIR = Path(ZS_DIR) / "results" / f"comics35_zs_{model_id.split('/')[1]}"
 
-# DATASET_DIR = os.path.join(ZS_DIR, "datasets")
-# OUTPUT_DIR = os.path.abspath(os.path.join(ZS_DIR, "results", f"""zs_{model_id.split("/")[1]}"""))
 
 
 ## 2. Instantiate Model and Tokenizer ###
@@ -44,20 +42,13 @@ OUTPUT_DIR = Path(ZS_DIR) / "results" / f"comics35_zs_{model_id.split('/')[1]}"
 inference_tokenizer = AutoTokenizer.from_pretrained(model_id, padding='left', padding_side='left')
 inference_tokenizer.pad_token = inference_tokenizer.eos_token
 terminators = [inference_tokenizer.eos_token_id, inference_tokenizer.convert_tokens_to_ids("<|eot_id|>")]
-#inference_tokenizer.chat_template = "llama"
+
 
 generation_model = AutoModelForCausalLM.from_pretrained(
     model_id,
     torch_dtype=torch.bfloat16,
-    #attn_implementation="flash_attention_2",
     device_map="auto",
 )
-
-#print(f"After model instantiation: {torch.cuda.memory_allocated(generation_model.device) / (1024**2)}")
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#generation_model.to(device)
-# print(generation_model.device)
-#generation_model = nn.DataParallel(generation_model)
 
 
 ### 3. Read Data from CSV file ###
@@ -70,26 +61,9 @@ df = df.drop(columns=[df.columns[0], df.columns[1]]).reset_index(drop=True)
 
 ### 4. Prepare Messages and Prompts ###
 
-# * Prompt Prepration for LLaMA and Qwen 
-# sys_msg_l = []
-# task_msg_l = []
-# prepared_sys_task_msg_l = []
-
-# for row in df.iterrows():
-
-#     sys_msg = {"role":"system", "content": "### Task description: You are an expert sentiment analysis assistant that takes an utterance from a comic book and must classify the utterance into appropriate emotion class(s): anger, surprise, fear, disgust, sadness, joy, neutral. You must absolutely not generate any text or explanation other than the following JSON format {\"utterance_emotion\": <predicted emotion classes for the utterance (str)>}\n\n"}
-#     task_msg = {"role":"user", "content": f"# Utterance:\n{row[1].utterance}\n\n# Result:\n"}
-
-#     sys_msg_l.append(sys_msg)
-#     task_msg_l.append(task_msg)
-
-# for i in range(len(sys_msg_l)):
-
-#     prepared_sys_task_msg_l.append([sys_msg_l[i], task_msg_l[i]])
-
-# * Prompt Prepration for Gemma
 
 def build_instruction():
+    
     emotion_classes = ["anger", "disgust", "fear", "sadness", "surprise", "joy", "neutral"]
     formatted_classes = ", ".join([f'"{emotion}"' for emotion in emotion_classes])
     
@@ -128,34 +102,25 @@ prepared_sys_task_msg_l = []
 
 for row in df.iterrows():
 
-    #top_msg = {"role": "system", "content": "### Task description: You are an expert sentiment analysis assistant that takes an utterance from a comic book and must classify the utterance into appropriate emotion class(s): anger, surprise, fear, disgust, sadness, joy, neutral. You must absolutely not generate any text or explanation other than the following JSON format: {\"utterance_emotion\": \"<predicted emotion classes for the utterance (str)>}\"\n\n"}
-    #sys_msg = {"role":"user", "content": instruction}
-    #task_msg = {"role":"assistant", "content": f"# Utterance:\n{row[1].utterance}\n\n# Result:\n"}
     sys_msg = {"role": "system", "content": build_instruction()}
     task_msg = {"role": "assistant", "content": f"""Input: "{row[1].utterance}"
 Output:"""}
 
     sys_msg_l.append(sys_msg)
     task_msg_l.append(task_msg)
-    #top_msg_l.append(top_msg)
+
 
 for i in range(len(sys_msg_l)):
 
     prepared_sys_task_msg_l.append([sys_msg_l[i], task_msg_l[i]])
-    #prepared_sys_task_msg_l.append([sys_msg_l[i]])
 
 ### 5. Run Classification / Generate labels ###
 
 outputs_l = []
-
-#for i in tqdm(range(len(prepared_sys_task_msg_l))):
-
 messages = prepared_sys_task_msg_l
 
 inputs = inference_tokenizer.apply_chat_template(
             messages,
-            #tools=tools,
-            # pad_token = tokenizer.eos_token,
             padding=True,
             truncation=True,
             add_generation_prompt=True,
@@ -163,13 +128,13 @@ inputs = inference_tokenizer.apply_chat_template(
             return_tensors="pt",
 )
 
+
 def batch_tensor(tensor, batch_size):
     return [tensor[i:i+batch_size] for i in range(0, tensor.size(0), batch_size)]
-
-
 BATCH_SIZE = 128
 input_ids_batches = batch_tensor(inputs['input_ids'], BATCH_SIZE) # type: ignore
 attention_mask_batches = batch_tensor(inputs['attention_mask'], BATCH_SIZE) # type: ignore
+
 
 generated_outputs = []
 
@@ -193,58 +158,9 @@ for i, (input_ids_batch, attention_mask_batch) in enumerate(zip(input_ids_batche
     generated_outputs.append(generated)
 
 
-# outputs = generation_model.generate(
-#     input_ids = input_ids,
-#     max_new_tokens=64,
-#     pad_token_id=inference_tokenizer.eos_token_id,
-#     eos_token_id=terminators,
-#     do_sample=True,
-#     temperature=0.1,
-#     top_p=0.9,
-#     )
-
-# for i in tqdm(range(len(prepared_sys_task_msg_l))):
-
-#     messages = prepared_sys_task_msg_l[i]
-
-#     input_ids = inference_tokenizer.apply_chat_template(
-#     messages,
-#     add_generation_prompt=True,
-#     tokenize=True,
-#     #tokenize=False,
-#     padding=True,
-#     truncation=True,
-#     #return_dict=True,
-#     return_tensors="pt"
-# )#.to(generation_model.device) 
-#     x = torch.tensor(input_ids).to(generation_model.device)
-#     #y = torch.tensor(input_ids["attention_mask"]).to(generation_model.device)
-#     #print(x)
-#     #print(y)
-#     #break
-#     #idz = x.to(generation_model.device)
-#     #x = torch.tensor(input_ids.)
-#     #x.to(generation_model.device)
-#     #print(x)
-#     #print(type(x))
-#     #print(x.device)
-#     #break
-#     outputs = generation_model.generate(
-#     input_ids = x,
-#     max_new_tokens=64,
-#     pad_token_id=inference_tokenizer.eos_token_id,
-#     eos_token_id=terminators,
-#     do_sample=True,
-#     temperature=0.1,
-#     top_p=0.9,
-#     )
-#     # inference_tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
-#     outputs_l.append(inference_tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True))  # type: ignore
-
 ### 6. Save Results, Post Process and Save Classficiation Reports ###
 
 grounds = df.emotions_list.tolist()
-#preds = [list(ast.literal_eval(output).values()) for output in outputs_l]
 
 decoded_outputs = []
 
@@ -265,50 +181,12 @@ with results_file.open('wb') as fh:
     pickle.dump(results_d, fh)
 
 
-# preds = []
-# for output in outputs_l:
-#     for prediction in output:
-#         try:
-#             # Use json.loads to safely parse the JSON-like string
-#             parsed_prediction = json.loads(prediction)
-#             # Append the values of the parsed prediction to preds
-#             preds.append(list(parsed_prediction.values()))
-#         except json.JSONDecodeError as e:
-#             print(f"Error decoding prediction: {e}")
-#             # Optionally, append a placeholder or handle error
-#             #preds.append([])  # or handle the error differently
+grounds_matrix, preds_matrix, classes = post_process(grounds, preds) # type: ignore
 
-# preds = []
-# for output in outputs_l:
-#     for prediction in output:
-#         preds.append([list(ast.literal_eval(prediction).values())])
+print(classification_report(grounds_matrix, preds_matrix, target_names=classes, digits=3))
 
-#preds = [list(ast.literal_eval(output).values()) for output in outputs_l]
+classification_file = Path(OUTPUT_DIR) / "classification_report.pickle"
 
-# preds = []
-
-# for output in outputs_l:
-
-#     print(len(output))
-
-#     for prediction in output:
-
-#         emotion_data = json.loads(prediction)
-        
-#         # Split by comma and strip any extra whitespace
-#         emotions = [emotion.strip() for emotion in emotion_data["utterance_emotion"].split(',')]
-        
-#         # Append each emotion list to the final list
-#         preds.append(emotions)
-
-# print(len(preds))
-
-# grounds_matrix, preds_matrix, classes = post_process(grounds, preds) # type: ignore
-
-# print(classification_report(grounds_matrix, preds_matrix, target_names=classes, digits=3))
-
-# classification_file = Path(OUTPUT_DIR) / "classification_report.pickle"
-
-# with classification_file.open('wb') as fh:
+with classification_file.open('wb') as fh:
     
-#     pickle.dump(classification_report(grounds_matrix, preds_matrix, target_names=classes, output_dict=True), fh)
+    pickle.dump(classification_report(grounds_matrix, preds_matrix, target_names=classes, output_dict=True), fh)
--- zeroshot/utils/post_processing.py
+++ zeroshot/utils/post_processing.py
@@ -1,35 +1,44 @@
 import json
 from sklearn.preprocessing import MultiLabelBinarizer
 
-def post_process(grounds, preds):
+def post_process(results):
     
-    grounds, predictions = extract_results(grounds, preds)
-    #predictions = harmonize_preds(predictions)
+    grounds, predictions = extract_results(results)
     grounds_matrix, predictions_matrix, classes = get_mlb(grounds, predictions)
     
     return grounds_matrix, predictions_matrix, classes
 
-def extract_results(grounds, preds):
+def extract_results(results):
     
-    #grounds = results["grounds"]
-    #predictions = results["predictions"]
-    grounds = grounds
-    predictions = preds
-    predictions = [json.loads(x["content"]) for x in predictions]
+    grounds = results["grounds"]
+    raw_predictions = results["predictions"]
+    
+    predictions = []
 
-    grounds = [json.loads(x)["list_emotion_classes"] for x in grounds]  
-    predictions = [x['list_emotion_classes'] for x in predictions]
-    
-    grounds = [['Neutral'] if elem == [['Neutral']] else elem for elem in grounds]
-    predictions = [['Neutral'] if elem == [['Neutral']] else elem for elem in predictions]
-    
-    return grounds, predictions
+    for pred in raw_predictions:
+        
+        predictions.append(pred.split("\n\n")[-1])
+        
+    predictions_l = []
+    non_matching_indices = []
 
-def harmonize_preds(predictions):
+    for i, pred in enumerate(predictions):
+        try:
+            predictions_l.append(json.loads(pred)["list_emotion_classes"])
+        except:
+            print(i)
+            non_matching_indices.append(i)  
+            
+    non_matching_indices.sort(reverse=True)
+
+# Remove elements from 'grounds' at the specified indices
+    for idx in non_matching_indices:
     
-    # TODO
+        del grounds[idx]
     
-    return predictions
+    
+    return grounds, predictions_l
+
     
 def get_mlb(grounds, predictions):
     
@@ -38,60 +47,3 @@ def get_mlb(grounds, predictions):
     predictions_mhot = mlb.transform(predictions)
     
     return grounds_mhot, predictions_mhot, mlb.classes_
-
-
-# import numpy as np
-
-# all_labels = ["anger", "surprise", "fear", "disgust", "sadness", "joy", "neutral"]
-
-# def labels_to_binary_matrix(label_list, all_labels):
-#     binary_matrix = np.zeros((len(label_list), len(all_labels)))
-#     for i, labels in enumerate(label_list):
-#         for label in labels:
-#             if label in all_labels:
-#                 binary_matrix[i][all_labels.index(label)] = 1
-#     return binary_matrix
-
-# def opposite(component_type):
-
-#     if component_type == "anger":
-#         return "surprise"
-#     elif component_type == "disgust":
-#         return "joy"
-#     elif component_type == "fear":
-#         return "sadness"
-#     elif component_type == "sadness":
-#         return "anger"
-#     elif component_type == "surprise":
-#         return "disgust"
-#     elif component_type == "joy":
-#         return "fear"
-#     elif component_type == "Neutral":
-#         return "sadness"
-    
-
-# def harmonize_preds(grounds, preds):
-
-#     l1, l2 = len(preds), len(grounds)
-#     if l1 < l2:
-#         diff = l2 - l1
-#         preds = preds + [opposite(x) for x in grounds[l1:]]
-#     else:
-#         preds = preds[:l2]
-        
-#     return preds 
-
-# def post_process_zs(grounds, preds):
-
-#     for i,(x,y) in enumerate(zip(grounds, preds)):
-        
-#         if len(x) != len(y):
-            
-#             preds[i] = harmonize_preds(x, y)
-
-#     true_matrix = labels_to_binary_matrix(grounds, all_labels)
-#     predicted_matrix = labels_to_binary_matrix(preds, all_labels)
-
-#     return true_matrix, predicted_matrix
-
-
